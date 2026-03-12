fb86ca6b144c95094e6d4a83a0d9780f282c3e16
Leakey-Hans <226905899+Leakey-Hans@users.noreply.github.com>
2026-03-12 11:17:29 +0300

    Etch-a-Sketch
    Added pending UI mobile responsiveness to be fixed later

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const gridContainer = document.querySelector(".containerDiv");
 const gridOptions = document.querySelector('.options');
 
+/*The Initial 16x16 grid
+Inside this div we keep creating a new div and appending it to the the parent  16 times*/
+for(let i = 1; i <= 256; i++){
+    const gridSquare = document.createElement("div");
+    /*Here our square div have a css height and Width of 40px 
+    meaning if we reduce the size of the container the hardcoded height 
+    and width will make the squares overflow outside I implement 
+    the square size for it to fit dynamicaly even if the container is*/
+    let squareSize = 100 / 16;
+    gridSquare.style.flexBasis = `${squareSize}%`
+    gridSquare.style.height= `${squareSize}%`
+    //Hover Effect
+    gridSquare.addEventListener("mouseover", () => {
+        gridSquare.classList.add("divHover");
+    }); 
+    gridContainer.appendChild(gridSquare);
+};
+
 const gridButton = document.createElement("button");
 gridButton.textContent = "Generate Grid";
 gridOptions.appendChild(gridButton);
+gridButton.classList.add("gridButton");
 
 
 gridButton.addEventListener("click", () => {
-    const newGrid = prompt("Enter Number Of Squares You Need between 1 - 100");
+    let newGrid = prompt("Enter Number Of Squares You Need between 1 - 100");
     gridContainer.innerHTML = "";   //Clears out the 16x16 grid
     if (newGrid > 100){
         alert('Thats Prolly too much');
-    };
-    let iterations = newGrid * newGrid;
-    for(let i = 1; i <= iterations; i++){
+        window.location.reload(); //reloads the page to default to the usual 16x16 grid
+    } else {
+        let iterations = newGrid * newGrid;
+        for(let i = 1; i <= iterations; i++){
         const gridSquare = document.createElement("div");
         /*In order for the square to keep fitting dynamically 
         let the container size bee 100% and divide it with newGrid to get 
@@ -27,17 +47,7 @@ gridButton.addEventListener("click", () => {
         }); 
         gridContainer.appendChild(gridSquare);
     }
+    }
 });
 
-
-/*The Initial 16x16 grid
-Inside this div we keep creating a new div and appending it to the the parent  16 times*/
-for(let i = 1; i <= 256; i++){
-    const gridSquare = document.createElement("div");
-    //Hover Effect
-    gridSquare.addEventListener("mouseover", () => {
-        gridSquare.classList.add("divHover");
-    }); 
-    gridContainer.appendChild(gridSquare);
-};
 
